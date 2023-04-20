5b60ed9fcd9caaad416b0469f51c337f01bb3f38
1260957445@qq.com <1260957445@qq.com>
2023-04-20 19:29:51 +0800

    g

--- onebox/检测更新.js
+++ onebox/检测更新.js
@@ -1,4 +1,4 @@
-<a class="版本">230419</a>
+<a class="版本">230420</a>
 <ul class="默认提示">
 <li><font color="blue"><b>已是最新版，本轻站适用最新版土狗浏览器</b></font>
 <p>mqqwpa://im/chat?chat_type=wpa&uin=1260957445</p>
@@ -8,7 +8,7 @@
 </li>
 </ul>
 <ul class="更新提示">
-<li><font color="red"><b>有新版本!!![修复布蕾影城嗅探]点击安装</b></font>
+<li><font color="red"><b>有新版本!!![修复批量搜索]点击安装</b></font>
 <p>mapi:qm/https://raw.fastgit.org/egwang186/iptv/main/onebox/一个盒子.mkz</p>
 </li>
 </ul>
