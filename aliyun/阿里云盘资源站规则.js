a64eb69d00cb8a290eb8db9fd6b745ddc1a16a4a
1260957445@qq.com <1260957445@qq.com>
2023-07-03 20:42:38 +0800

    g

--- aliyun/阿里云盘资源站规则.js
+++ aliyun/阿里云盘资源站规则.js
@@ -23,7 +23,7 @@
     "rule":{
         "分类":'var a=JSON.parse(getHttp(JSON.stringify({url:"http://a.gitcafe.net/home.json",head:{"Referer":"http://a.gitcafe.net/","User-Agent":"okhttp/4.2.2"}}))).data;var xindata=[];for(var i in a){xindata.push({title:a[i].name+a[i].cat_num,url:"https://gitcafe.net/tool/alipaper/$$action=viewcat&num=#PN#&cat="+a[i].code})}JSON.stringify(xindata);',
         "翻页数据":'var b="";for(var i=1;i<1000;i=i+1){b=b+"+第"+i+"页="+i;}"翻页"+b;',
-        "列表规则":'var 列表=e2Arr(getCode(),".json(data)");var 地址规则=".json(alikey)";var 标题规则=".json(title)";var 图片规则=".json(tok)";var 简介规则=".(分享者:).json(aliuser).c(<br>分享时间:).json(creatime)";var NEXTPAGE="";var PREPAGE="";',
+        "列表规则":'var 列表=e2Arr(getCode(),".json(data)");var 地址规则=".json(alikey)";var 标题规则=".json(title)";var 图片规则=".json(tok)";var 简介规则=".c(分享者:).json(aliuser).c(<br>分享时间:).json(creatime)";var NEXTPAGE="";var PREPAGE="";',
         "详情规则":'var 正文=e2Rex(getVar("源"),".json(title).c(链接:https://www.aliyundrive.com/s/).json(url).c(<br>).json(detail)");'
     }
 },
