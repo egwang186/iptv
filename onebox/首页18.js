d73b4847f9db935daae91e110f28da12fbf45694
1260957445@qq.com <1260957445@qq.com>
2023-08-01 00:53:59 +0800

    g

--- onebox/首页18.js
+++ onebox/首页18.js
@@ -139,14 +139,14 @@ var code=getHttp(getVar("baseURL")+"/cat");var arr=e2Arr(code,".get(div.grid).i(
 ##分类规则
 var 列表=e2Arr(getVar("源码"),".get(div.shadow.p-1)");var 标题规则=".get(img).a(alt)";var 地址规则=".get(a).a(href)";var 图片规则=".get(img).a(src)";var 简介规则=".get(div.absolute).st().t()";var 图片底部规则="";var 左上规则="";var 右上规则="";var NEXTPAGE="";var PREPAGE="";
 ##选集规则
-var apicode=getHttp(getVar("地址").replace('/v/','/api/v/'));var 分类=[];分类.push(apicode);var 简介="";var 线路="";var 列表规则=".json(video)";var 标题规则=".var(标题)";var 选集规则=".var(标题)";var 选集地址规则=".json(videoUrl)";
+var apicode=getHttp(getVar("地址").replace('/v/','/api/v/'));var 分类=[];分类.push(apicode);var 简介="";var 线路="";var 列表规则=".json(video)";var 标题规则=".var(标题)";var 选集规则=".var(标题)";var 选集地址规则="@js=\"http://ip111.cn/?wd=\"+e2(列表[j],\".json(videoUrl)\").replace(/\\?/g,\"幻魔快修\");";
 ##搜索规则
 var 源码=getVar("源码");var 列表=e2Arr(源码,".get(div.shadow.p-1)");if(!列表[0]){var 列表=["网页可能有搜索验证，去网页看看吧"];var 标题规则=".t()";var 地址规则=getVar("baseURL");var 图片规则=".get(img).a(data-original)";var 简介规则="长按网页打开，验证后返回重新搜索即可";var 图片底部规则="";var 左上规则="";var 右上规则="";var NEXTPAGE="";var PREPAGE="";}else{var 标题规则=".get(img).a(alt)";var 地址规则=".get(a).a(href)";var 图片规则=".get(img).a(src)";var 简介规则=".get(div.absolute).st().t()";var 图片底部规则="";var 左上规则="";var 右上规则="";var NEXTPAGE="";var PREPAGE="";}
 ##搜索翻页
 var b="";for(var i=1;i<50;i=i+1){b=b+"+第"+i+"页="+i;}
 "翻页"+b;
 ##免嗅探规则
-var 头=getVar("地址").split("/hls")[0];var code=getHttp(getVar("地址")).match(/#EXT-X-STREAM-INF[^#]+/g);var playlist=[];for(var i in code){var title=code[i].match(/RESOLUTION=(.+)/)[1];var url=头+"/"+code[i].match(/hls.+/)[0];playlist.push({name:title,url:url});}JSON.stringify(playlist);
+var 头=getVar("地址").split("?wd=")[1].replace(/幻魔快修/g,"?").split("/hls")[0];var code=getHttp(getVar("地址").split("?wd=")[1].replace(/幻魔快修/g,"?")).match(/#EXT-X-STREAM-INF[^#]+/g);var playlist=[];for(var i in code){var title=code[i].match(/RESOLUTION=(.+)/)[1];var url=头+"/"+code[i].match(/hls.+/)[0];playlist.push({name:title,url:url});}JSON.stringify(playlist);
 ####
 
 ####
