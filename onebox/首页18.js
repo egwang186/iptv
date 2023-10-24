58c889a22f4d873d101a0e140aab0d2ba400a76e
1260957445@qq.com <1260957445@qq.com>
2023-10-24 09:58:41 +0800

    g

--- onebox/首页18.js
+++ onebox/首页18.js
@@ -351,13 +351,15 @@ getVar("baseURL")+"/zh/category/分类/翻页.json";
 getVar("baseURL")+"/zh/search/关键字/翻页.json";
 ###rule
 ##首页规则
-var 列表=e2Arr(getVar("源码"),".z(\\{\"id\":.+?totalView.+?\\})");var 标题规则=".json(name)";var 地址规则=".c(/zh/video/).json(id).ct(.json)";var 图片规则="@js=var id=JSON.parse(CODE).id;var 前=id.split('-')[0];var 后=id.split('-')[1];if(id.indexOf('FC2-')==0){'https://ccavb.tv/static/img/'+前+'/'+后.substr(0,2)+'/'+后+'/'+id+'_1.jpg'}else{'https://ccavb.tv/static/img/'+前+'/'+后+'/'+id+'_1.jpg'}";var 简介规则=".json(types)";var 图片底部规则=".json(updateTime)";var 左上规则="";var 右上规则="";var NEXTPAGE="";var PREPAGE="";
+var 预列表=e2Arr(getVar("源码"),".json(pageProps).json(resp)");var 列表=[];for(var i in 预列表){if(e2Arr(预列表[i],".json(content)").length>1){列表=列表.concat(e2Arr(预列表[i],".json(content)"))}}
+var 标题规则=".json(name)";var 地址规则=".c(/zh/video/).json(id).ct(.json)";var 图片规则="@js=var id=JSON.parse(CODE).id;var 前=id.split('-')[0];var 后=id.split('-')[1];if(id.indexOf('FC2-')==0){'https://ccavb.tv/static/img/'+前+'/'+后.substr(0,2)+'/'+后+'/'+id+'_1.jpg'}else{'https://ccavb.tv/static/img/'+前+'/'+后+'/'+id+'_1.jpg'}";var 简介规则=".json(types)";var 图片底部规则=".json(updateTime)";var 左上规则="";var 右上规则="";var NEXTPAGE="";var PREPAGE="";
 ##筛选数据
 var a="分类+最新上傳=最新上傳+熱門影片=熱門影片";var b="";for(var i=1;i<999;i++){b=b+"+第"+i+"页="+i;}var b="翻页"+b;a+"\n"+b;
 ##分类规则
-var 列表=e2Arr(getVar("源码"),".z(\\{\"id\":.+?totalView.+?\\})");var 标题规则=".json(name)";var 地址规则=".c(/zh/video/).json(id).ct(.json)";var 图片规则="@js=var id=JSON.parse(CODE).id;var 前=id.split('-')[0];var 后=id.split('-')[1];if(id.indexOf('FC2-')==0){'https://ccavb.tv/static/img/'+前+'/'+后.substr(0,2)+'/'+后+'/'+id+'_1.jpg'}else{'https://ccavb.tv/static/img/'+前+'/'+后+'/'+id+'_1.jpg'}";var 简介规则=".json(types)";var 图片底部规则=".json(updateTime)";var 左上规则="";var 右上规则="";var NEXTPAGE="";var PREPAGE="";
+var 预列表=e2Arr(getVar("源码"),".json(pageProps).json(resp)");var 列表=[];for(var i in 预列表){if(e2Arr(预列表[i],".json(content)").length>1){列表=列表.concat(e2Arr(预列表[i],".json(content)"))}}
+var 标题规则=".json(name)";var 地址规则=".c(/zh/video/).json(id).ct(.json)";var 图片规则="@js=var id=JSON.parse(CODE).id;var 前=id.split('-')[0];var 后=id.split('-')[1];if(id.indexOf('FC2-')==0){'https://ccavb.tv/static/img/'+前+'/'+后.substr(0,2)+'/'+后+'/'+id+'_1.jpg'}else{'https://ccavb.tv/static/img/'+前+'/'+后+'/'+id+'_1.jpg'}";var 简介规则=".json(types)";var 图片底部规则=".json(updateTime)";var 左上规则="";var 右上规则="";var NEXTPAGE="";var PREPAGE="";
 ##选集规则
-var 分类=e2Arr(getVar("源码"),".json(pageProps)");var 线路="";var 简介="";var 列表规则=".json(data).i(1).z(http.+)";var 标题规则=getVar("标题");var 选集规则=".var(标题)";var 选集地址规则=".t()";
+var 分类=e2Arr(getVar("源码"),".json(pageProps)");var 线路="";var 简介="";var 列表规则=".json(resp).i(-1).z(http.+)";var 标题规则=getVar("标题");var 选集规则=".var(标题)";var 选集地址规则=".t()";
 ##搜索规则
 var 源码=getVar("源码");var 列表=e2Arr(getVar("源码"),".z(\\{\"aneID\":.+?\\})");if(!列表[0]){var 列表=["网页可能有搜索验证，去网页看看吧"];var 标题规则=".t()";var 地址规则=getVar("baseURL");var 图片规则=".get(img).a(data-original)";var 简介规则="长按网页打开，验证后返回刷新即可";var 图片底部规则="";var 左上规则="";var 右上规则="";var NEXTPAGE="";var PREPAGE="";}else{var 标题规则=".json(videoName)";var 地址规则=".c(/zh/video/).json(aneID).ct(.json)";var 图片规则="@js=var id=JSON.parse(CODE).aneID;var 前=id.split('-')[0];var 后=id.split('-')[1];if(id.indexOf('FC2-')==0){'https://ccavb.tv/static/img/'+前+'/'+后.substr(0,2)+'/'+后+'/'+id+'_1.jpg'}else{'https://ccavb.tv/static/img/'+前+'/'+后+'/'+id+'_1.jpg'}";var 简介规则=".json(videoType)";var 图片底部规则=".json(airDate)";var 左上规则="";var 右上规则="";var NEXTPAGE="";var PREPAGE="";}
 ##搜索翻页
