666dd7ad104a12092bceb4743e1f1a6d2c936cd4
1260957445@qq.com <1260957445@qq.com>
2023-11-19 01:17:19 +0800

    g

--- aliyun/阿里云盘资源站规则.js
+++ aliyun/阿里云盘资源站规则.js
@@ -41,7 +41,7 @@
     "rule":{
         "分类":'var a="影视音乐$tab=ppx5bp&subId=ppx5bp&startrow=33#PN#$$电视剧,纪录片$tab=qvnx1e&subId=qvnx1e&startrow=33#PN#$$动漫漫画$tab=kwi9g7&subId=kwi9g7&startrow=33#PN#$$小说.电子书.图片$tab=1nylx9&subId=1nylx9&startrow=33#PN#$$教程.课程.软件$tab=BB08J2&subId=BB08J2&startrow=33#PN#";var a=a.split("$$");var padId="300000000$TzIVdaOubWOP";var items=[];for(var i in a){var title=a[i].split("$")[0];var url="https://docs.qq.com/dop-api/get/sheet?"+a[i].split("$")[1]+"&padId="+padId+"&outformat=1";items.push({title:title,url:url});}JSON.stringify(items);',
         "翻页数据":'"翻页+全部="',
-        "列表规则":'var 原始=JSON.parse(getCode().replace(/"(\\d+)"/g, \'"qq_$1"\')).data.initialAttributedText.text[0][0][0].c[1];var list=[];for(var key in 原始){if(原始[key].qq_2&&原始[key].qq_2[1].length>1){list.push(原始[key].qq_2);}};var 列表=[];for(var i=0;i<list.length;i=i+3){var title="["+list[i][1]+"]"+list[i+1][1];var url=list[i][1];var detail=title+" 链接:"+list[i+2][1];列表.push(JSON.stringify({title:title,url:url,detail:detail}));}var 地址规则=".json(url)";var 标题规则=".json(title)";var 图片规则=".json(url)";var 简介规则=".json(detail)";var NEXTPAGE="";var PREPAGE="";',
+        "列表规则":'var 原始=JSON.parse(getCode().replace(/"(\\d+)"/g, \'"qq_$1"\')).data.initialAttributedText.text[0][0][0].c[1];var list=[];for(var key in 原始){if(原始[key].qq_2&&原始[key].qq_2[1].length>1){list.push(原始[key].qq_2);}};var 列表=[];for(var i=0;i+2<list.length;i=i+3){var title="["+list[i][1]+"]"+list[i+1][1];var url=list[i][1];var detail=title+" 链接:"+list[i+2][1];列表.push(JSON.stringify({title:title,url:url,detail:detail}));}var 地址规则=".json(url)";var 标题规则=".json(title)";var 图片规则=".json(url)";var 简介规则=".json(detail)";var NEXTPAGE="";var PREPAGE="";',
         "详情规则":'var 正文=getVar("简介");'
     }
 }
@@ -71,16 +71,16 @@ function 通用列表(){
 eval(getVar("列表规则"));通用列表();
 ######详情列表
 function 正文处理(){
-    var list=正文.match(/[\s\S]{2,30}?https:\/\/www\.aliyundrive\.com\/s\/.+/g);
+    var list=正文.match(/[\s\S]{2,30}?https:\/\/www\.ali.+?\.com\/s\/.+/g);
 var items=[];
 for(var i in list){
-    var title=list[i].replace(/.+?阿里云盘.+/g,"").replace(/<.+?>/g,"").split("https://www.aliyundrive.com/s/")[0]||"没获取到标题，去看原文吧";
+    var title=list[i].replace(/.+?阿里云盘.+/g,"").replace(/<.+?>/g,"").split("https://www.ali")[0]||"没获取到标题，去看原文吧";
     if(title.length>40){
         title=title.replace(/\s/g,"").substr(title.length-40);
     }
-    var share_id=list[i].match(/aliyundrive\.com\/s\/([0-9a-zA-Z]+)/)[1];
+    var share_id=list[i].match(/\.com\/s\/([0-9a-zA-Z]+)/)[1];
     if(list[i].search(/\/s\/.+?\/folder\/.+/)!=-1){
-        file_id=list[i].match(/aliyundrive\.com\/s\/.+?\/folder\/([0-9a-zA-Z]+)/)[1];
+        file_id=list[i].match(/\.com\/s\/.+?\/folder\/([0-9a-zA-Z]+)/)[1];
     }else{
         file_id="root";
     }
